271918a5b1b741cef305e12b66db975585ef23dc
Millena Kappaun <kappaunmillena@gmail.com>
2025-10-27 21:29:25 -0300

    Adicionando setState na aplicação

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -73,12 +73,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _addicionarTarefa() {
     var tarefaDigitada = controller.text;
-    if(tarefaDigitada.trim().isEmpty){
+    if (tarefaDigitada.trim().isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text("Você precisa digitar uma tarefa")),
       );
       return;
     }
-    tarefas.add(tarefaDigitada);
+    setState(() {
+      tarefas.add(tarefaDigitada);
+    });
   }
 }
